767e62d9433e556f9877cfb5fe912df033ea8b08
DEEPAK SAINI <111223168+Deepak05910@users.noreply.github.com>
2022-08-16 16:11:14 +0530

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+
+    int b;
+    cin>>b;
+
+    int c;
+    cin>>c;
+
+    int sum=a+b+c;
+    cout<<"total amount is"<<endl;
+    cout<<sum;
+    return 0;
+}
